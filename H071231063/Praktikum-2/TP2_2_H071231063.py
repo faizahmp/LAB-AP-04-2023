c55d019afed28ed1c2d2a3f277e6885e3dc7c264
Amaliaadiah <amaliadiahramadani@gmail.com>
2023-09-21 15:20:07 +0800

    ganti

--- H071231063/Praktikum-2/TP2_2_H071231063.py
+++ H071231063/Praktikum-2/TP2_2_H071231063.py
@@ -2,27 +2,23 @@
 input_pertama = input("Masukkan Input Pertama : ")
 input_kedua = input("Masukkan Input Kedua : ")
 input_ketiga = input("Masukkan Input Ketiga : ")
-# Menampilkan hashasilil atau pesan "Invalid input"
-def main_case(input_pertama, input_kedua, input_ketiga):
-    # Membuat kamus (dictionary) yang berisi informasi sesuai dengan pola yang diberikan
-    pola = {
-        ("vertebrado", "ave", "carnivoro"): "aguia",
-        ("vertebrado", "ave", "onivoro"): "pomba",
-        ("vertebrado", "mamifero", "onivoro"): "homem",
-        ("vertebrado", "mamifero", "herbivoro"): "vaca",
-        ("invertebrado", "inseto", "hematofago"): "pulga",
-        ("invertebrado", "inseto", "herbivoro"): "lagarta",
-        ("invertebrado", "anelideo", "hematofago"): "sanguessuga",
-        ("invertebrado", "anelideo", "onivoro"): "minhoca"
-    }
-
-    # Menggunakan tuple input sebagai kunci untuk mencari hasil
-    kunci = (input_pertama, input_kedua, input_ketiga)
-    if kunci in pola:
-        return pola[kunci]
-    else:
-        return "Invalid input"
-# Memanggil main_case untuk mendapatkan hasil
-hasil = main_case(input_pertama, input_kedua, input_ketiga)
 # Menampilkan hasil atau pesan "Invalid input"
-print(hasil)
+match (input_pertama, input_kedua, input_ketiga):
+    case "vertebrado", "ave", "carnivoro": 
+        print("aguia")
+    case "vertebrado", "ave", "onivoro":
+        print("pomba")
+    case "vertebrado", "mamifero", "onivoro":
+        print("homem")
+    case "vertebrado", "mamifero", "herbivoro": 
+       print("vaca")
+    case "invertebrado", "inseto", "hematofago":
+        print("pulga")
+    case "invertebrado", "inseto", "herbivoro":
+        print("lagarta")
+    case "invertebrado", "anelideo", "hematofago":
+        print("sanguessuga")
+    case "invertebrado", "anelideo", "onivoro":
+        print("minhoca")
+    case _:
+        print('invalid')
