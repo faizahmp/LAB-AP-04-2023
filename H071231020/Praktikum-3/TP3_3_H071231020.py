67dd5d6812e5807bd8a7942f286336091a4c683e
anggasisfo <anggasisfoangga@gmail.com>
2023-09-29 21:40:28 +0800

    kumpul tugas 3

---
+++ H071231020/Praktikum-3/TP3_3_H071231020.py
@@ -0,0 +1,26 @@
+while True:
+    try:
+        posisi = float(input())
+     
+        detik = (posisi / 360) * 24 * 3600
+        jam = (detik // 3600) + 6 #ditambah 6 karena 0 derajat pada jam 6
+        if jam >= 24:
+            jam %= 24 #jika melebihi jam 24:00:00 akan mulai lagi jam 00:00:00
+        detik = detik % 3600
+        menit = detik // 60
+        detik = detik % 60
+
+        if 6 <= jam <= 10 :
+            print('Selamat Pagi')
+        elif 11 <= jam < 15:
+            print('Selamat Siang')
+        elif 16 <= jam < 18:
+            print('Selamat Sore')
+        elif 18 <= jam < 24 or 0 <= jam < 6:
+            print('Selamat Malam') 
+
+        print(f'{jam:02.0f}:{menit:02.0f}:{detik:02.0f}')
+
+    except ValueError:
+        print('EOF')
+        break
