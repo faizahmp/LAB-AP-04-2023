ade0b2245f1b8be7cf22185ab90893b075850b2d
novireazra <novireazra@gmail.com>
2023-11-09 14:57:26 +0800

    Praktikum-9

---
+++ H071231069/Praktikum-9/TP9_2_H071231069.py
@@ -0,0 +1,32 @@
+class Mahasiswa:
+    def __init__(self, nama, nim, jurusan, ipk):
+        self.nama = nama
+        self.nim = nim
+        self.jurusan = jurusan
+        self.ipk = ipk
+
+    def info(self):
+        print('nama     : ', self.nama)
+        print('nim      : ', self.nim)
+        print('jurusan  : ', self.jurusan)
+        print('ipk      : ', self.ipk)
+
+    def predikat(self):
+        if self.ipk >= 3.5 :
+            return "Cumlaude"
+        elif self.ipk >= 3.0 :
+            return "Sangat Memuaskan"
+        elif self.ipk >= 2.5 :
+            return "Memuaskan"
+        elif self.ipk >= 2.0 :
+            return "Cukup"
+        elif self.ipk < 2.0 :
+            return "Kurang"
+        
+mahasiswa1 = Mahasiswa("Novi", "H071231069", "Sistem Informasi", 4.5 )
+mahasiswa1.info()
+print("predikat : ", mahasiswa1.predikat())
+
+# mahasiswa2 = Mahasiswa("Sabil", "H071231080", "Sistem Informasi", 3.5 )
+# mahasiswa2.info()
+# print("predikat : ", mahasiswa2.predikat())
