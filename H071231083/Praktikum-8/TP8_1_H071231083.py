350a1ebd49bad3729eb90bdedc9ea58bed663111
faizehsan <ehsanfaiss04@gmail.com>
2023-11-10 18:52:03 +0800

    Kumpul TP8

---
+++ H071231083/Praktikum-8/TP8_1_H071231083.py
@@ -0,0 +1,11 @@
+import re
+
+def validate_file(input_string):
+    pattern= r'^[a-zA-Z2468]{40}[13579\s]{5}$'
+    if re.match(pattern, input_string):
+        return True
+    else:
+        return False
+input_string= input("masukkan str: ")
+hasil= validate_file(input_string)
+print(hasil)
